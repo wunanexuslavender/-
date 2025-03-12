3bfaf3824c71b5ef5eb66bd811e2278e73a3740a
wunanexuslavender <nexuslavender@gmail.com>
2025-03-12 22:26:16 +0800

    3月12日的学习

---
+++ 模仿老师的上课程序3月12日.py
@@ -0,0 +1,34 @@
+import math
+#import keyword
+#print(keyword.kwlist)
+
+#a=int(input("请输入莲华:"))
+#c=a+1
+#print(c)
+
+#x=3
+#y=3
+#print(x is y)
+
+#x='我爱莲华'
+#y='我爱莲华'
+#print(x is y)
+
+#a=9
+#print(bin(a))
+#print(oct(a))
+#print(hex(a))
+
+#a=9.23
+#print(a.as_integer_ratio())#转化为分数
+#print(a.is_integer())#判断小数
+
+#print(type(1+2j))
+#print(type("int"))
+#print(int(9.72))
+#print(float(8))
+#print(str(100))
+
+#print(ord('A'))
+#print(chr(24355))
+
