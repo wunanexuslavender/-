759c4a6b1b51925def8e456bb605ccae99f027fc
wunanexuslavender <nexuslavender@gmail.com>
2025-03-13 21:14:03 +0800

    今天一些小知识点

---
+++ 今天的一些小知识点.py
@@ -0,0 +1,14 @@
+s1=set('lianhua')
+s2=set('lian')
+print(s1)
+print(s1&s2)
+for n in 'lianhua':
+    print(n)
+g={'lianhua':520}
+print(g['lianhua'])
+s3={9,2,3,5,2,0}#集合中的元素就算你打了重复的他也不计
+print(s3)
+s4={'lianhua','lian','hua'}
+
+
+
