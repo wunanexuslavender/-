63f04e999ec772f52cee586df3e329854cd0af7d
wunanexuslavender <nexuslavender@gmail.com>
2025-03-16 11:59:57 +0800

    基础包都没安好啊，太难了

---
+++ 模仿学长们项目思路第一节3月16日.py
@@ -0,0 +1,23 @@
+#知识点见右边一行
+#(def data_imp():                             定义一个函数 def a(): return a+b
+#    data1=[40,100]#此处省略列表中数据            unexpected indent意外的缩进
+#    data1[0]=14#质量数为14 省略所有赋值)
+#这只是一个数据集 还有多个
+                                              #.pth文件是pytorch深度学习框架中格式
+#import torch                                  #引入torch包  引入其他原先没带的包
+#import torch.nn as nn
+#import torch.optim as optim
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import random
+#from sklearn.preprocessing import StandardScalar
+#from sklearn.model_selection import train_test_split
+
+#定义神经网络模型
+class NeuralNetwork(nn.Module):
+
+
+
+
+
