c220b8e001946741711e266aac6012cf44176863
wunanexuslavender <nexuslavender@gmail.com>
2025-03-20 22:19:15 +0800

    3月20日上课

---
+++ 3月20日上课听课.py
@@ -0,0 +1,32 @@
+print('我爱莲华')
+#for n in range(10):
+#    print('我爱莲华')
+
+#c=str.upper('lianHua')   #str.lower
+#print(c)
+
+#\\输出\
+
+#print(r'lianhua\n')      #\\,\t等
+#print('lianhua\n')
+
+#print(str.islower('lianhua'))
+#p='莲华'
+#print(p.isalnum())   #isdigit  isspace
+
+a='lianhua'
+#print(a.find('a'))
+
+s=('莲华,丛雨,沙耶')
+#print(str.split(','))
+#print(rsplkit())
+
+#table1=str.maketrans('1234567','我爱莲华一辈子')
+#s1='234'
+#s2=s1.translate(table1)
+#print(s2)
+
+#s2='我爱lianhua'
+#s1=s2.replace('lianhua','莲华')
+#print(s1)
+
