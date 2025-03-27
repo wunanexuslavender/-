a1ba7db8f71d027dc1cdea21fb8284a5051f0499
wunanexuslavender <nexuslavender@gmail.com>
2025-03-27 09:57:49 +0800

    3月27日上课

---
+++ 3月27日听课上.py
@@ -0,0 +1,37 @@
+#def lianhua(x,y=8):                   #别忘了加冒号
+#    return x+y
+
+
+#a=int(input('请输入第一个数:'))
+#b=int(input('请输入第二个数:'))
+#print(lianhua(a,b))
+
+#def empty(x,y):
+#    pass                             #占位函数
+
+
+#print('{:.2f}+{:.2f}={:.2f}'.format(a,lianhua(x=3)))
+
+    #形参加一个*意味可变形参元组类型dtup，两个为字典类型ddic的可变参数            例如
+#def lianhua(d,*dtup,**ddic):
+#    print('必选参数:',d)
+#    print('元祖类型可变参数:',dtup)
+#    print('字典类型可变参数:',ddic)    #这里不用加*
+#    for k in ddic:
+#        print('{}对应{}'.format(k,ddic[k]))
+
+#lianhua(1,2,3,4,5,6,x=10,y=20,z=30)
+
+#def lianhua(a,b):
+#    if b==0:
+#        return a
+#    else:
+#@        return lianhua(b,a%b)
+#b=162
+#b=189
+#print('%d与%d的最大公约数是%d'%(a,b,lianhua(a,b)))
+
+
+
+
+
